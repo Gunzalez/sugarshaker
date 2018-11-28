7a4b5f5976617957c1e32ae803ea44d0f6478686
Segun Konibire <gunzalez@hotmail.com>
2018-11-28 21:33:47 +0000

    adjust shrinking header

--- website/js/sugarshaker.js
+++ website/js/sugarshaker.js
@@ -5,6 +5,13 @@ DomReady.ready(function() {
     };
 
     function scrollFunction() {
+
+        if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+            document.getElementsByTagName("body")[0].classList.add('fixed');
+        } else {
+            document.getElementsByTagName("body")[0].classList.remove('fixed');
+        }
+
         if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
             document.getElementById("header").classList.add('smaller');
         } else {
