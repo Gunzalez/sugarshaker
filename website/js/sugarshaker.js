0dd2c3b9a27dabe7df9091792f8112f0e60a9862
Segun Konibire <gunzalez@hotmail.com>
2018-11-29 20:23:22 +0000

    basic tabs section working

--- website/js/sugarshaker.js
+++ website/js/sugarshaker.js
@@ -1,23 +1,54 @@
 DomReady.ready(function() {
 
-    window.onscroll = function() {
-        scrollFunction();
+    var sugarShaker = {};
+
+    sugarShaker.environment = {
+
+        scrollFunction: function () {
+            if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+                document.getElementsByTagName("body")[0].classList.add('fixed');
+            } else {
+                document.getElementsByTagName("body")[0].classList.remove('fixed');
+            }
+
+            if (document.body.scrollTop > 25 || document.documentElement.scrollTop > 25 ) {
+                document.getElementById("header").classList.add('smaller');
+            } else {
+                document.getElementById("header").classList.remove('smaller');
+            }
+        }
+
     };
 
-    function scrollFunction() {
+    sugarShaker.tabs = {
 
-        if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-            document.getElementsByTagName("body")[0].classList.add('fixed');
-        } else {
-            document.getElementsByTagName("body")[0].classList.remove('fixed');
+        setUpEvents: function () {
+            var links = document.querySelectorAll('#tabs .tab');
+            for(var l=0; l < links.length; l++){
+                links[l].onclick = function (e) {
+                    e.preventDefault();
+                    if(!e.target.classList.contains('active')){
+
+                        document.querySelectorAll('#tabs .active')[0].classList.remove('active');
+                        e.target.classList.add('active');
+
+                        document.querySelectorAll('#tabs-content .active')[0].classList.remove('active');
+                        var index = [].slice.call(document.querySelectorAll('#tabs .tab')).indexOf(e.target);
+
+                        document.querySelectorAll('#tabs-content .tab-content')[index].classList.add('active');
+                    }
+                };
+            }
         }
+    };
 
-        if (document.body.scrollTop > 25 || document.documentElement.scrollTop > 25 ) {
-            document.getElementById("header").classList.add('smaller');
-        } else {
-            document.getElementById("header").classList.remove('smaller');
-        }
-    }
+    sugarShaker.tabs.setUpEvents();
 
+
+
+
+    window.onscroll = function() {
+        sugarShaker.environment.scrollFunction();
+    };
 
 });
