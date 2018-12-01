2d364f863087585a92fb808f7329308206e58804
Segun Konibire <gunzalez@hotmail.com>
2018-12-01 08:21:26 +0000

    adds menu html

--- website/js/sugarshaker.js
+++ website/js/sugarshaker.js
@@ -2,7 +2,7 @@ DomReady.ready(function() {
 
     var sugarShaker = {};
 
-    sugarShaker.environment = {
+    sugarShaker.header = {
 
         scrollFunction: function () {
             if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
@@ -17,35 +17,63 @@ DomReady.ready(function() {
                 document.getElementById("header").classList.remove('smaller');
             }
         }
-
     };
 
     sugarShaker.tabs = {
 
         setUpEvents: function () {
             var links = document.querySelectorAll('#tabs .tab');
-            for(var l=0; l < links.length; l++){
-                links[l].onclick = function (e) {
-                    e.preventDefault();
-                    if(!e.target.classList.contains('active')){
+            if(links){
+                for(var l=0; l < links.length; l++){
+                    links[l].onclick = function (e) {
+                        e.preventDefault();
+                        if(!e.target.classList.contains('active')){
 
-                        document.querySelectorAll('#tabs .active')[0].classList.remove('active');
-                        e.target.classList.add('active');
+                            document.querySelectorAll('#tabs .active')[0].classList.remove('active');
+                            e.target.classList.add('active');
 
-                        document.querySelectorAll('#tabs-content .active')[0].classList.remove('active');
-                        var index = [].slice.call(document.querySelectorAll('#tabs .tab')).indexOf(e.target);
+                            document.querySelectorAll('#tabs-content .active')[0].classList.remove('active');
+                            var index = [].slice.call(document.querySelectorAll('#tabs .tab')).indexOf(e.target);
 
-                        document.querySelectorAll('#tabs-content .tab-content')[index].classList.add('active');
-                    }
-                };
+                            document.querySelectorAll('#tabs-content .tab-content')[index].classList.add('active');
+                        }
+                    };
+                }
             }
         }
     };
 
+    sugarShaker.navigation = {
+
+        init: function () {
+            var trigger = document.querySelector('#trigger');
+            if(trigger){
+                trigger.addEventListener('click', function (e) {
+                    e.preventDefault();
+                    sugarShaker.navigation.show();
+                });
+            }
+        },
+
+        show: function () {
+            var navigation = document.querySelector('#page-navigation');
+            if(navigation){
+                navigation.classList.add('show');
+                document.body.classList.add('no-scroll');
+            }
+        },
+
+        scroll: function () {
+
+        }
+    };
+
+    sugarShaker.navigation.init();
+
     sugarShaker.tabs.setUpEvents();
 
     window.onscroll = function() {
-        sugarShaker.environment.scrollFunction();
+        sugarShaker.header.scrollFunction();
     };
 
 });
